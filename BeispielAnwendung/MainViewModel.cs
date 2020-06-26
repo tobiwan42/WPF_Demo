72ffd2b6c1f18dcc7590f4cba121602d4b1d320f
Tobias Kuch <kuch@hm.edu>
2020-06-26 16:51:47 +0200

    changed filenames

--- BeispielAnwendung/MainViewModel.cs
+++ BeispielAnwendung/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace BeispielAnwendung
 {
+    using System;
+    using System.Windows.Media.Animation;
     using OxyPlot;
     using OxyPlot.Series;
 
@@ -8,11 +10,21 @@
         public MainViewModel()
         {
             this.MyModel = new PlotModel { Title = "Example 1" };
-            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+            ItemsSeries sinus = new FunctionSeries(Math.Cos, -5, 5, 0.1, "cos(x)");
+            ItemsSeries cosinus = new FunctionSeries(Math.Sin, -5, 5, 0.1, "sin(x)");
+            this.MyModel.Series.Add(sinus);
+            this.MyModel.Series.Add(cosinus);
+            //this.MyModel.Series.Clear();
+        }
+
+        public void SetGraph(ItemsSeries series)
+        {
+            this.MyModel.Series.Clear();
+            this.MyModel.Series.Add(series);
+            this.MyModel.InvalidatePlot(true);
         }
 
         public PlotModel MyModel { get; private set; }
     }
 }
-
 
